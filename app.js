d6d9fd2783060abf673191741266418ebb85186e
nucknine <nucknine@gmail.com>
2018-08-27 14:53:57 +0300

    my version comleted

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var vm = new Vue({
         monsterHp: 100,
         dmgToPlayer: 0,
         dmgToMonster: 0,
+        healAmount: 10,
         logs: []
     },
     methods: {
         start() {
+            this.logs = [];
             this.myHp = 100;
             this.monsterHp = 100;
             this.started = true;
@@ -23,7 +25,8 @@ var vm = new Vue({
                 me: this.dmgToPlayer,
                 monster: this.dmgToMonster
             }
-            this.logs.push(log)
+            this.logs.push(log);
+            this.checkHp();
         },
         specialAttack(){
             this.dmgToPlayer = Math.floor(Math.random() * 40) + 1;
@@ -34,21 +37,47 @@ var vm = new Vue({
                 me: this.dmgToPlayer,
                 monster: this.dmgToMonster
             }
-            this.logs.push(log)
+            this.logs.push(log);
+            this.checkHp();
         },
         heal() {
             this.dmgToPlayer = Math.floor(Math.random() * 20) + 1;
-            this.dmgToMonster = Math.floor(Math.random() * 20) + 1;
-            this.myHp += this.dmgToPlayer;
-            this.myHp -= this.dmgToMonster;
+            this.dmgToMonster = 0;
+            this.myHp += this.healAmount;
+            this.myHp -= this.dmgToPlayer;
             let log = {
+                meHeal: this.healAmount,
                 me: this.dmgToPlayer,
-                monster: this.dmgToMonster
+                monster: false
             }
             this.logs.push(log)
+            this.checkHp();
         },
         giveUp() {
             this.started = false;
+            this.myHp = 100;
+            this.monsterHp = 100;
+            this.logs = [];
+        },
+        checkHp(){
+            if(this.myHp <= 0 ) {
+                if(window.confirm('You have lost! Wanna play Again?')) {
+                    this.start();
+                } else {
+                    this.giveUp()
+                };
+            } else if (this.myHp > 100 ){
+                this.myHp = 100;
+            }
+            if( this.monsterHp <= 0 ) {
+                if(window.confirm('Congrats, pal, you have won! Wanna play Again?')) {
+                    this.start();
+                } else {
+                    this.giveUp();
+                };
+            } else if (this.myHp > 100 ){
+                this.monsterHp = 100;
+            }
         }
     },
     computed: {
