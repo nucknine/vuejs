3c74016c1f26ac72032fa835faecbb14c2bf6e8a
nucknine <nucknine@gmail.com>
2018-09-12 12:03:23 +0300

    start

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>{{msg}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      msg: 'hello'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
