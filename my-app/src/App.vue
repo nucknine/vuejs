175ce113a5632efc66b4d95bf86935b62623bee1
nucknine <nucknine@gmail.com>
2018-09-14 17:04:38 +0300

    http tests

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,14 +1,97 @@
 <template>
-  <div>{{msg}}</div>
+  <div class="container">
+    <div class="row">
+      <div class="offset-2 col-sm-8">
+        <div class="form-group">
+          <label for="">Node name</label>
+          <input type="text" v-model="node">
+        </div>
+        <div class="form-group">
+          <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" v-model="user.name">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="text" class="form-control" id="email" v-model="user.email">
+        </div>
+        <button class="btn btn-default btn-primary" @click="submit" >Submit</button>
+        <hr>
+        <button class="btn btn-default btn-primary" @click="fetchData" >Fetch</button>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="(u, i) in users" :key="u + i">{{u.name + '-' + u.email}}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'app',
-  data() {
+  data () {
     return {
-      msg: 'hello'
+      user: {
+        name: '',
+        email: ''
+      },
+      users: [],
+      resource: {},
+      node: 'data',
+      // node: 'alternative'
+    }
+  },
+  created () {
+    let customActions = {
+      getData: { method: 'GET' }
+    }
+    this.resource = this.$resource('{node}.json', {}, customActions)
+  },
+  methods: {
+    submit () {
+      this.$http.post('alternative.json', this.user)
+        .then(response => {
+          console.log(response)
+        }, error => {
+          console.log(error)
+        })
+    },
+    fetchData () {
+      // через http
+      // this.$http.get('data.json')
+      //   .then(res => res.json())
+      //   .then(res => {
+      //     for (let key in res) {
+      //       this.users.push(res[key])
+      //     }
+      //   })
+
+      // через resource
+      // this.resource.get('')
+      //   .then(res => res.json())
+      //   .then(res => {
+      //     for (let key in res) {
+      //       this.users.push(res[key])
+      //     }
+      //   })
+
+      // через "resource customAction" getData
+      // this.resource.getData('')
+      //   .then(res => res.json())
+      //   .then(res => {
+      //     for (let key in res) {
+      //       this.users.push(res[key])
+      //     }
+      //   })
+
+      // через "resource customAction" getData с dynamic path
+      this.resource.getData({ node: this.node })
+        .then(res => res.json())
+        .then(res => {
+          for (let key in res) {
+            this.users.push(res[key])
+          }
+        })
     }
   },
   components: {
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
+Vue.use(VueResource)
+Vue.http.options.root = 'https://vuejs-http-bfa8c.firebaseio.com/'
 Vue.config.productionTip = false
 
 new Vue({
